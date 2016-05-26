57a29eeb3a393772bbc9825ffe8eead2db2e8599
chris erway <cce@appneta.com>
2016-05-26 11:11:45 -0400

    Add noboe_test.go

---
+++ v1/tv/internal/traceview/noboe_test.go
@@ -0,0 +1,28 @@
+// +build !traceview
+
+// Copyright (C) 2016 AppNeta, Inc. All rights reserved.
+
+package traceview
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNoboe(t *testing.T) {
+	// no tracing if build tag not enabled
+	globalReporter = &nullReporter{}
+	sampled, _, _ := shouldTraceRequest("test", "")
+	assert.False(t, sampled)
+	globalReporter = newReporter()
+	sampled, _, _ = shouldTraceRequest("test", "")
+	assert.False(t, sampled)
+
+	r := SetTestReporter()
+	sampled, _, _ = shouldTraceRequest("test", "")
+	assert.True(t, sampled)
+	r.ShouldTrace = false
+	sampled, _, _ = shouldTraceRequest("test", "")
+	assert.False(t, sampled)
+}
