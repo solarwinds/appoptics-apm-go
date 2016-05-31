6df19224a99957b30a6a6ea175fe55fbd85dc7c2
chris erway <cce@appneta.com>
2016-05-31 00:51:46 -0400

    Revert struct{} nullTrace change   ~/go/src/github.com/appneta/go-appneta/v1/tv$ go test -bench Benchmark -benchmem   PASS   BenchmarkNewTrace-8     30000000                36.7 ns/op             0 B/op          0 allocs/op   ok                      github.com/appneta/go-appneta/v1/tv            1.374s

--- v1/tv/context.go
+++ v1/tv/context.go
@@ -34,7 +34,7 @@ func fromContext(ctx context.Context) (l Layer, ok bool) {
 func TraceFromContext(ctx context.Context) Trace {
 	t, ok := traceFromContext(ctx)
 	if !ok {
-		return nullTrace{}
+		return &nullTrace{}
 	}
 	return t
 }
