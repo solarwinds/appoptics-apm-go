2020a6811e536752be1d3b004df5c15a5d99aea1
[Eric Yang] <eric@librato.com>
2017-07-31 12:41:48 -0700

    added transaction names limit checking and other methods

--- v1/tv/internal/traceview/metrics.go
+++ v1/tv/internal/traceview/metrics.go
@@ -6,9 +6,10 @@ import "time"
 
 const (
 	MetricsRecordMaxSize = 100
+	MaxTransactionNames = 200
 )
 // MetricsAggregator processes the metrics records and calculate the metrics and
-// histogram message from them.
+// histograms message from them.
 type MetricsAggregator interface {
 	// FlushBSON requests a metrics message from the message channel and encode
 	// it into a BSON message.
@@ -28,22 +29,21 @@ type metricsAggregator struct {
 	rawReq chan struct{}
 	raw chan MetricsRaw
 	exit chan struct{}
-	MetricsRaw
+	transNames map[string]bool
+	metrics MetricsRaw
 }
 
 type MetricsRaw struct {
-	histogram Histograms
-	Measurements
+	histograms map[string]Histogram
+	measurements Measurements
 }
 
-type histogram struct {
+type baseHistogram struct {
 	// TODO: use the hdr library
 }
 
-type Histograms map[string]Histogram
-
 type Histogram struct {
-	hist histogram
+	data baseHistogram
 	tags map[string]string
 }
 
@@ -59,7 +59,7 @@ type MetricsRecord struct {
 	HasError bool
 }
 
-// FlushBSON is called by the reporter to generate the histogram/metrics
+// FlushBSON is called by the reporter to generate the histograms/metrics
 // message in BSON format. It send a request to the histReq channel and
 // blocked in the hist channel. FlushBSON is called synchronous so it
 // expects to get the result in a short time.
@@ -67,7 +67,13 @@ func (am *metricsAggregator) FlushBSON() [][]byte {
 	am.rawReq <- struct{}{}
 	// Don't let me get blocked here too long
 	raw := <- am.raw
-	//TODO: encode hist
+	return am.createMetricsMsg(raw)
+}
+
+// createMetricsMsg read the histogram and measurement data from MetricsRaw and build
+// the BSON message.
+func (am *metricsAggregator) createMetricsMsg(raw MetricsRaw) [][]byte {
+	// TODO
 }
 
 // ProcessMetrics consumes the records sent by traces and update the histograms.
@@ -88,27 +94,58 @@ func (am *metricsAggregator) ProcessMetrics() {
 	}
 }
 
-func (am *metricsAggregator) updateMetricsRaw(record MetricsRecord) {
-	am.recordHistogram("", record.Duration)
-	// TODO
+// isWithinLimit stores the transaction name into a internal set and returns true, before that
+// it checks if the number of transaction names stored inside metricsAggregator is still within
+// the limit. If not it returns false and does not store the transaction name.
+func (am *metricsAggregator) isWithinLimit(transaction string, max int) bool {
+	if _, ok := am.transNames[transaction]; !ok {
+		if len(am.transNames) < max {
+			am.transNames[transaction] = true
+			return true
+		} else {
+			return false
+		}
+	}
+	return true
 }
 
+// metricsAggregator updates the Metrics (histograms and measurements) raw data structs based on
+// the MetricsRecord.
+func (am *metricsAggregator) updateMetricsRaw(record MetricsRecord) {
+	am.recordHistogram("", record.Duration)
+	if record.Transaction {
+		if am.isWithinLimit(record.Transaction, MaxTransactionNames) {
+			am.recordHistogram(record.Transaction, record.Duration)
+			am.processMeasurements(record.Transaction, record)
+		} else {
+			am.processMeasurements("other", record)
+		}
+	} else {
+		am.processMeasurements("unknown", record)
+	}
+}
+
+// recordHistogram updates the histogram based on the new MetricsRecord (transaction name and
+// the duration).
 func (am *metricsAggregator) recordHistogram(transaction string, duration time.Duration) {
 	// TODO
 }
 
+// processMeasurements updates the measurements struct based on the new MetricsRecord
 func (am *metricsAggregator) processMeasurements(transaction string, record MetricsRecord) {
 	// TODO
 }
 
-// pushMetricsRaw is called when FlushBSON requires a new histogram message
-// for encoding. It push the newest values of the histogram to the hist channel
+// pushMetricsRaw is called when FlushBSON requires a new histograms message
+// for encoding. It pushes the newest values of the histograms to the raw channel
 // which will be consumed by FlushBSON.
 func (am *metricsAggregator) pushMetricsRaw() {
 	// TODO
+	am.raw <- am.metrics
 }
 
-//TODO: return error types
+// PushMetricsRecord is called by the Trace to record the metadata of a call, e.g., call duration,
+// transaction name, status code.
 func (am *metricsAggregator) PushMetricsRecord(record MetricsRecord) bool {
 	select {
 	case am.records <- record:
@@ -118,11 +155,16 @@ func (am *metricsAggregator) PushMetricsRecord(record MetricsRecord) bool {
 	}
 }
 
+// newMetricsAggregator is the newMetricsAggregator initializer. Note: You still need to
+// initialize the Hisogram.data each time you add a new key/value to it, as by default
+// it's a nil map pointer.
 func newMetricsAggregator() MetricsAggregator {
 	return &metricsAggregator{
 		records: make(chan MetricsRecord, MetricsRecordMaxSize),
 		rawReq: make(chan struct{}),
 		raw: make(chan MetricsRaw),
 		exit: make(chan struct{}),
+		transNames: make(map[string]bool),
+		metrics: MetricsRaw{histograms: make(map[string]Histogram)},
 	}
 }
