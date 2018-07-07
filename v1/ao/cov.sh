71e4ca50b1b542b9aa481e79a5d0c31e18003b18
Jiwen Yang <jiwen624@foxmail.com>
2018-07-06 17:14:17 -0700

    fix a cov.sh covermode conflicts with -race error

--- v1/ao/cov.sh
+++ v1/ao/cov.sh
@@ -3,22 +3,22 @@ set -e
 
 COVERPKG="github.com/appoptics/appoptics-apm-go/v1/ao/internal/reporter,github.com/appoptics/appoptics-apm-go/v1/ao/internal/agent,github.com/appoptics/appoptics-apm-go/v1/ao,github.com/appoptics/appoptics-apm-go/v1/ao/opentracing"
 export APPOPTICS_DEBUG_LEVEL=1
-go test -v -race -covermode=count -coverprofile=cov.out -coverpkg $COVERPKG
-go test -v -race -tags disable_tracing -covermode=count -coverprofile=covao.out -coverpkg $COVERPKG
+go test -v -race -covermode=atomic -coverprofile=cov.out -coverpkg $COVERPKG
+go test -v -race -tags disable_tracing -covermode=atomic -coverprofile=covao.out -coverpkg $COVERPKG
 
 pushd internal/reporter/
-go test -v -race -covermode=count -coverprofile=cov.out
-go test -v -race -tags disable_tracing -covermode=count -coverprofile=covao.out
+go test -v -race -covermode=atomic -coverprofile=cov.out
+go test -v -race -tags disable_tracing -covermode=atomic -coverprofile=covao.out
 popd
 
 pushd internal/agent/
-go test -v -race -covermode=count -coverprofile=cov.out
-go test -v -race -tags disable_tracing -covermode=count -coverprofile=covao.out
+go test -v -race -covermode=atomic -coverprofile=cov.out
+go test -v -race -tags disable_tracing -covermode=atomic -coverprofile=covao.out
 popd
 
 pushd opentracing
-go test -v -race -covermode=count -coverprofile=cov.out
-go test -v -race -tags disable_tracing -covermode=count -coverprofile=covao.out
+go test -v -race -covermode=atomic -coverprofile=cov.out
+go test -v -race -tags disable_tracing -covermode=atomic -coverprofile=covao.out
 popd
 
 gocovmerge cov.out covao.out internal/reporter/cov.out internal/reporter/covao.out internal/agent/cov.out internal/agent/covao.out opentracing/cov.out opentracing/covao.out > covmerge.out
