832a7f4240794ef86123a2bccc93014e7ee0c00d
jiwen624 <jiwen624@gmail.com>
2018-10-04 10:26:59 -0700

    add SetOperationName to span

--- v1/ao/opentracing/tracer.go
+++ v1/ao/opentracing/tracer.go
@@ -182,7 +182,7 @@ func (s *spanImpl) SetOperationName(operationName string) ot.Span {
 	s.Lock()
 	defer s.Unlock()
 
-	s.context.span.SetTransactionName(operationName)
+	s.context.span.SetOperationName(operationName)
 	return s
 }
 
