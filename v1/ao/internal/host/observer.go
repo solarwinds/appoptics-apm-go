d18280b017c9e66e2cafe1355768633731a80a85
jiwen624 <jiwen624@gmail.com>
2018-11-19 14:38:34 -0800

    add test case for dyno ID initializer

--- v1/ao/internal/host/observer.go
+++ v1/ao/internal/host/observer.go
@@ -246,11 +246,15 @@ func getMACAddressList() []string {
 
 func getHerokuDynoId() string {
 	dynoOnce.Do(func() {
-		if d, has := os.LookupEnv(envDyno); has {
-			dyno = d
-		} else {
-			dyno = ""
-		}
+		initDyno(&dyno)
 	})
 	return dyno
+}
+
+func initDyno(dyno *string) {
+	if d, has := os.LookupEnv(envDyno); has {
+		*dyno = d
+	} else {
+		*dyno = ""
+	}
 }
