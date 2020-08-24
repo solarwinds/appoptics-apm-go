5f7a0bc64fccd71fd631a37632460d94866e5eae
chris erway <cce@solarwinds.cloud>
2020-08-24 17:47:26 -0400

    Have multitracer_test.go set up two tracers

--- v1/ao/opentracing/multitracer_test.go
+++ v1/ao/opentracing/multitracer_test.go
@@ -3,9 +3,9 @@ package opentracing
 import (
 	"testing"
 
-	//	aoot "github.com/appoptics/appoptics-apm-go/v1/ao/opentracing"
 	"github.com/appoptics/appoptics-apm-go/v1/ao/internal/reporter"
 	mt "github.com/appoptics/appoptics-apm-go/v1/contrib/multitracer"
+	bt "github.com/opentracing/basictracer-go"
 	opentracing "github.com/opentracing/opentracing-go"
 	"github.com/opentracing/opentracing-go/harness"
 )
@@ -13,7 +13,14 @@ import (
 func TestMultiTracerAPICheck(t *testing.T) {
 	_ = reporter.SetTestReporter(reporter.TestReporterDisableDefaultSetting(true)) // set up test reporter
 	harness.RunAPIChecks(t, func() (tracer opentracing.Tracer, closer func()) {
-		return &mt.MultiTracer{Tracers: []opentracing.Tracer{NewTracer()}}, nil
+		return &mt.MultiTracer{
+			Tracers: []opentracing.Tracer{
+				NewTracer(),
+				bt.NewWithOptions(bt.Options{
+					Recorder:     bt.NewInMemoryRecorder(),
+					ShouldSample: func(traceID uint64) bool { return true }, // always sample
+				}),
+			}}, nil
 	},
 		harness.CheckBaggageValues(false),
 		harness.CheckInject(true),
