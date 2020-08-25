163dd3f6f9c5a030f66a8e36cdc809a22aa63bb8
chris erway <cce@solarwinds.cloud>
2020-08-25 10:16:27 -0400

    Add AO+basictracer MultiTracer test using build flag (to keep it out of go modules)

--- v1/ao/opentracing/multitracer_two_test.go
+++ v1/ao/opentracing/multitracer_two_test.go
@@ -1,3 +1,7 @@
+// +build basictracer
+//
+// Behind a build tag avoid adding a dependency on basictracer-go
+
 package opentracing
 
 import (
@@ -5,17 +9,25 @@ import (
 
 	"github.com/appoptics/appoptics-apm-go/v1/ao/internal/reporter"
 	mt "github.com/appoptics/appoptics-apm-go/v1/contrib/multitracer"
+	bt "github.com/opentracing/basictracer-go"
 	opentracing "github.com/opentracing/opentracing-go"
 	"github.com/opentracing/opentracing-go/harness"
 )
 
-func TestMultiTracerAPICheck(t *testing.T) {
+// This test sets up AO and the OT "BasicTracer" side by side
+func TestMultiTracerBasicTracerAPICheck(t *testing.T) {
 	_ = reporter.SetTestReporter(reporter.TestReporterDisableDefaultSetting(true)) // set up test reporter
 	harness.RunAPIChecks(t, func() (tracer opentracing.Tracer, closer func()) {
-		return &mt.MultiTracer{Tracers: []opentracing.Tracer{NewTracer()}}, nil
+		return &mt.MultiTracer{
+			Tracers: []opentracing.Tracer{
+				NewTracer(),
+				bt.NewWithOptions(bt.Options{
+					Recorder:     bt.NewInMemoryRecorder(),
+					ShouldSample: func(traceID uint64) bool { return true }, // always sample
+				}),
+			}}, nil
 	},
 		harness.CheckBaggageValues(false),
 		harness.CheckInject(true),
-		harness.CheckExtract(true),
 	)
 }
