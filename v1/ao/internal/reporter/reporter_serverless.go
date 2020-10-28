038bbbf1482901bb50c8e1dd1476fcf7f94eb7d0
jiwen624 <jiwen624@gmail.com>
2020-10-28 09:21:20 -0700

    [serverless] remove int with underscore

--- v1/ao/internal/reporter/reporter_serverless.go
+++ v1/ao/internal/reporter/reporter_serverless.go
@@ -23,7 +23,7 @@ func newServerlessReporter(writer io.Writer) reporter {
 		customMetrics: metrics.NewMeasurements(true, 0, 500),
 	}
 
-	r.logWriter = newLogWriter(false, writer, 260_000)
+	r.logWriter = newLogWriter(false, writer, 260000)
 
 	updateSetting(int32(TYPE_DEFAULT),
 		"",
